29ff7630070c7f0e14f2d00c59df62f1450c12dc
imufazalov <iskamuf24smurf@gmail.com>
2020-09-25 13:40:44 +0300

    Добавил тестов для всех символов в токенайзере

--- AdvancedClac/Program.cs
+++ AdvancedClac/Program.cs
@@ -7,13 +7,13 @@ namespace AdvancedClac
         static void Main(string[] args)
         {
             Tokenizer TL = new Tokenizer();
-           foreach (var ystem in TL.Scan("13"))
+           foreach (var ystem in TL.Scan("sin(45)"))
             {
                 Console.WriteLine(ystem.Value);
                 Console.WriteLine(ystem.TokenType);
             }
             
-            //onsole.WriteLine(MathFunc.Eval(MathFunc.Parsing(TL.Scan("0.2+4"))));
+            //сonsole.WriteLine(MathFunc.Eval(MathFunc.Parsing(TL.Scan("0.2+4"))));
         }
     }
 }
